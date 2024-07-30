0851d533298faaf448377c457260cde2a654a9ad
Uzair Alam <a-8278@kmclu.ac.in>
2024-07-30 20:41:13 +0530

    initial tailwind, react and vite setup

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='min-h-screen w-screen bg-zinc-900 text-white'>
+      Hello World
+    </div>
+  )
+}
+
+export default App
