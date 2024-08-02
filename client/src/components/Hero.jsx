f8560c29158babd09c0f438724d7c4770653d3a2
Uzair Alam <a-8278@kmclu.ac.in>
2024-08-02 15:50:54 +0530

    add:hover effect

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,40 +1,50 @@
-import { motion } from "framer-motion";
 import React from "react";
 import CountUp from "react-countup";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { carouselImages, tags } from "../utils";
 import Carousel from "./cards/Carousel";
+import { animateHover, removeHover } from "../utils/services";
 
 const Hero = () => {
   const theme = useSelector((state) => state.theme.value);
   return (
     <div className="page1 bg-gradient-to-r from-transparent to-slate-900/30 md:h-screen w-full flex max-lg:flex-col gap-6">
       <div className="heroLeft h-full flex flex-col items-center justify-center w-1/2 max-lg:w-full">
-        <div className="flex leftContent items-start justify-center flex-col md:w-3/5 px-4 gap-4">
-          <h1>Welcome to Jobster</h1>
-          <h1 className="text-5xl max-lg:text-3xl font-bold">
+        <div
+          className="relative flex leftContent items-start justify-center flex-col md:w-3/4 px-4 gap-4 max-lg:mt-[10vh]"
+          onMouseMove={(e) => animateHover(e)}
+          onMouseLeave={() => removeHover()}
+        >
+          <h1 className="z-[12]">Welcome to Jobster</h1>
+          <div className="absolute h-1/3 overlayGif md:w-1/2 w-full inset-0 z-[11] scale-75">
+            <img
+              src={"./images/hoverGif.gif"}
+              className="overlayGifImg absolute md:w-[30vw] h-[40vh] max-lg:scale-50 max-lg:-ml-20 max-lg:-mt-20 -mt-12 max-lg:aspect-square opacity-75  rounded-full"
+            />
+          </div>
+          <h1 className="text-6xl max-lg:text-3xl font-bold stroke md:mix-blend-difference z-[12]">
             Find the Perfect Job for You.
           </h1>
-          <p className="text-sm max-lg:text-xs">
+          <p className="text-sm max-lg:text-xs z-[12]">
             Search your career opportunities through{" "}
             <CountUp end={28000} duration={5} suffix="+" /> jobs
           </p>
           <div
-            className={`rounded-2xl flex border border-slate-500 max-lg:flex-col gap-1 p-2 px-4 max-lg:w-full ${
+            className={`rounded-2xl flex border border-slate-500 z-[12] max-lg:flex-col gap-1 p-2 px-4 max-lg:w-full ${
               theme === "dark" && "bg-slate-900/90"
             }`}
           >
             <input
               type="text"
               placeholder="Search for jobs"
-              className="bg-transparent outline-none w-full text-sm py-2"
+              className="bg-transparent outline-none w-full text-sm py-2 z-[12]"
             />
-            <div className="flex items-center justify-between gap-4">
+            <div className="flex items-center justify-between gap-4 z-[12]">
               <select
                 name="location"
                 id=""
-                className="bg-gray-400/30 border border-slate-500 rounded-md p-1 text-sm font-light outline-none"
+                className="bg-gray-400/30 border z-[12] border-slate-500 rounded-md p-1 text-sm font-light outline-none"
               >
                 <option
                   className={` ${
@@ -72,13 +82,20 @@ const Hero = () => {
           </div>
           <p className="">Popular Searches</p>
           <div className="flex flex-wrap gap-2 space-y-1 text-xs">
-          {tags.map((tag, index) => {
-            return (
-              <span key={index} className={`flex items-center rounded-xl px-2 py-[1px] border ${theme === 'dark' ? 'bg-slate-900/90' : 'bg-gray-300'} hover:bg-gray-800 cursor-pointer`}>
-                {tag}
-              </span>
-            );
-          })}
+            {tags.map((tag, index) => {
+              return (
+                <span
+                  key={index}
+                  className={`flex items-center rounded-xl px-2 py-[1px] z-[12] border ${
+                    theme === "dark"
+                      ? "bg-slate-900/90 hover:bg-gray-800"
+                      : "bg-gray-300 hover:bg-gray-500"
+                  } cursor-pointer`}
+                >
+                  {tag}
+                </span>
+              );
+            })}
           </div>
         </div>
       </div>
@@ -87,7 +104,7 @@ const Hero = () => {
         <div className="cards h-1/2 aspect-square relative flex items-center justify-center ">
           <Carousel autoSlide autoSlideInterval={5000}>
             {carouselImages.map((image, index) => (
-              <img src={image} key={index}/>
+              <img src={image} key={index} />
             ))}
           </Carousel>
         </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({children}) => {
   return (
-    <nav className="fixed top-0 z-[10] border-b  backdrop-blur-md w-screen px-2 md:px-8 py-3 flex items-center justify-between">
+    <nav className="fixed top-0 z-[99] border-b  backdrop-blur-md w-screen px-2 md:px-8 py-3 flex items-center justify-between">
       <Link to='/' className="logo text-xl font-semibold">Jobster</Link>
       <div className="navRight flex items-center justify-end gap-8 max-sm:gap-2">
       {children}
